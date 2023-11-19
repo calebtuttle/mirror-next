46d6472c318f48405b1c672ab5123889b233e419
calebtuttle <1calebtuttle@gmail.com>
2023-11-19 13:42:32 -0500

    add basic silk login

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,15 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { initSilk } from '@silk-wallet/silk-wallet-sdk'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import ThemeContext from '@/context/theme'
 
 const Layout = ({ publication, children }) => {
 	if (publication.theme.colorMode === 'DARK' && typeof document !== 'undefined') document.body.classList.add('dark')
+
+	const { connector: activeConnector, isConnected, address } = useAccount()
+  const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
+	const { disconnect } = useDisconnect()
 
 	return (
 		<>
@@ -25,18 +31,51 @@ const Layout = ({ publication, children }) => {
 				<div className={publication.theme.colorMode === 'DARK' ? 'dark' : ''}>
 					<div className="dark:bg-gray-900 min-h-screen">
 						<header className="p-4">
-							<Link href="/">
-								<a className="flex items-center space-x-4">
-									<img className="ring-1 ring-gray-200 dark:ring-gray-700 rounded-full w-14 h-14 hover:ring-4 transition ease-in-out" src={publication.avatarURL} />
-									<div>
-										<h1 className="text-xl font-medium dark:text-gray-200">{publication.displayName}</h1>
-										<div className="flex items-center space-x-2">
-											<span className="rounded-full bg-gray-200 dark:bg-gray-800 px-1 text-sm text-gray-500 font-medium">ENS</span>
-											<p className="text-gray-400 dark:text-gray-600 pb-0.5">{process.env.NEXT_PUBLIC_AUTHOR_ENS}</p>
+							<div className="flex justify-between items-center space-x-4">
+								<Link href="/">
+									<div className="hover:cursor-pointer flex items-center space-x-4">
+										<img className="ring-1 ring-gray-200 dark:ring-gray-700 rounded-full w-14 h-14 hover:ring-4 transition ease-in-out" src={publication.avatarURL} />
+										<div>
+											<a>
+												<h1 className="text-xl font-medium dark:text-gray-200">{publication.displayName}</h1>
+												<div className="flex items-center space-x-2">
+													<span className="rounded-full bg-gray-200 dark:bg-gray-800 px-1 text-sm text-gray-500 font-medium">ENS</span>
+													<p className="text-gray-400 dark:text-gray-600 pb-0.5">{process.env.NEXT_PUBLIC_AUTHOR_ENS}</p>
+												</div>
+											</a>
 										</div>
 									</div>
-								</a>
-							</Link>
+								</Link>
+								{isConnected && address ? (
+									<div>
+										<p className="text-sm font-medium dark:text-gray-200">{address}</p>
+										<hr />
+										<button 
+											className="float-right text-gray-300 hover:underline font-bold rounded"
+											onClick={() => disconnect()}
+										>
+											Disconnect
+										</button>
+									</div>
+								) : (
+									<button 
+										className="bg-gray-300 text-gray-900 hover:bg-gray-200 font-bold py-2 px-4 rounded"
+										onClick={async () => {
+											try {
+												await window.ethereum.login()
+												connect({
+													connector: connectors[0],
+													chainId: 1,
+												})
+											} catch (err) {
+												console.error(err)
+											}
+										}}
+									>
+										Login
+									</button>
+								)}
+							</div>
 						</header>
 						<main className="max-w-3xl mx-auto py-16 px-4 sm:px-0">{children}</main>
 					</div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,22 @@ import NProgress from 'nprogress'
 import { useRouter } from 'next/router'
 import '@/styles/style.css'
 import { useEffect } from 'react'
+import { initSilk } from '@silk-wallet/silk-wallet-sdk'
 import Layout from '@/components/Layout'
 import * as Fathom from 'fathom-client'
+import { configureChains, mainnet, createConfig, WagmiConfig } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet],
+  [publicProvider()],
+)
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+})
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter()
@@ -11,19 +25,22 @@ function MyApp({ Component, pageProps }) {
 	NProgress.configure({ showSpinner: false })
 
 	useEffect(() => {
-		Fathom.load('STJUQNYD', {
-			includedDomains: ['m1guelpf.blog'],
-			url: 'https://hyena.m1guelpf.blog/script.js',
-		})
+		// Fathom.load('STJUQNYD', {
+		// 	includedDomains: ['m1guelpf.blog'],
+		// 	url: 'https://hyena.m1guelpf.blog/script.js',
+		// })
 
-		// Record a pageview when route changes
-		router.events.on('routeChangeComplete', () => Fathom.trackPageview())
+		// // Record a pageview when route changes
+		// router.events.on('routeChangeComplete', () => Fathom.trackPageview())
 
-		// Unassign event listener
-		return () => {
-			router.events.off('routeChangeComplete', () => Fathom.trackPageview())
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+		// // Unassign event listener
+		// return () => {
+		// 	router.events.off('routeChangeComplete', () => Fathom.trackPageview())
+		// }
+		// // eslint-disable-next-line react-hooks/exhaustive-deps
+
+		const silk = initSilk()
+		window.ethereum = silk
 	}, [])
 
 	useEffect(() => {
@@ -41,9 +58,11 @@ function MyApp({ Component, pageProps }) {
 	if (Component.hasLayout === false) return <Component {...pageProps} />
 
 	return (
-		<Layout {...pageProps}>
-			<Component {...pageProps} />
-		</Layout>
+		<WagmiConfig config={config}>
+			<Layout {...pageProps}>
+				<Component {...pageProps} />
+			</Layout>
+		</WagmiConfig>
 	)
 }
 
